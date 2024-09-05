cf997357b962c48c9796b6a8079055324afb0191
Matthias Matthies <matmat@uni-bremen.de>
2024-09-05 13:41:49 +0200

    vielleicht ist noch falsch, da nicht die erste sondern letzte instanz gewählt wird

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+fn binary_search<T: std::cmp::PartialOrd + std::fmt::Debug>(needle: T, haystack: &Vec<T>) -> Option<usize> {
+    return binary_search_help(needle, haystack, 0, haystack.len());
+}
+
+fn binary_search_help<T: std::cmp::PartialOrd + std::fmt::Debug>(needle: T, haystack: &Vec<T>, left_bound: usize, right_bound: usize) -> Option<usize> {
+    let middle = ((right_bound - left_bound) / 2) + left_bound;
+    if right_bound - left_bound <= 2 {
+        return if haystack[middle] == needle {
+            Some(middle)
+        } else {
+            None
+        }
+    }
+    return if haystack[middle] > needle {
+        binary_search_help(needle, haystack, left_bound, middle)
+    } else {
+        binary_search_help(needle, haystack, middle, right_bound)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut ordered: Vec<i32> = Vec::new();
+    ordered.push(1);
+    ordered.push(2);
+    ordered.push(3);
+    ordered.push(3);
+    ordered.push(3);
+    ordered.push(3);
+    ordered.push(4);
+    ordered.push(5);
+    ordered.push(6);
+    ordered.push(7);
+    ordered.push(8);
+    let needle = 3;
+    let bin = binary_search(needle, &ordered);
+    println!("{:#?}", bin);
+    println!("Hello World");
 }
